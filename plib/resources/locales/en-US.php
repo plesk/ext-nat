471176f540a789a2eecc9c19d3dda6e051af2535
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-07-10 15:29:23 +0700

    More details on how to propagete changes.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -2,7 +2,7 @@
 
 $messages = array(
     'indexPageTitle' => 'IP Addresses for NAT',
-    'indexPageHint' => 'Select IP address and define public to private IP translation.',
+    'indexPageHint' => 'Select IP address and define public to private IP translation. In order to propagate changes to DNS zones of existing domains need to sync these zones with DNS zone template.',
     'updateAddressPageTitle' => 'IP Address Traslation',
     'publicIp' => 'Public IP',
     'mainIp' => 'Main (or private IP)',
