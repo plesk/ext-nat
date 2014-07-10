4b62203230d97860f15ce9f949288d6865671fbd
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-07-10 17:37:42 +0700

    Fix typo.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -3,7 +3,7 @@
 $messages = array(
     'indexPageTitle' => 'NAT Manager',
     'indexPageHint' => 'Select IP address and define public to private IP translation. In order to propagate changes to DNS zones of existing domains need to sync these zones with DNS zone template.',
-    'updateAddressPageTitle' => 'IP Address Traslation',
+    'updateAddressPageTitle' => 'IP Address Translation',
     'publicIp' => 'Public IP',
     'mainIp' => 'Main (or private IP)',
     'ipUpdated' => 'IP address was successfully updated.',
