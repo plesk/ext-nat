17a8c5d56dea3d88a3dac0a7f1ac7d1c44502b0a
Alexei Yuzhakov <ayuzhakov@parallels.com>
2014-07-10 15:34:21 +0700

    Better title for main screen.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,7 +1,7 @@
 <?php
 
 $messages = array(
-    'indexPageTitle' => 'IP Addresses for NAT',
+    'indexPageTitle' => 'NAT Manager',
     'indexPageHint' => 'Select IP address and define public to private IP translation. In order to propagate changes to DNS zones of existing domains need to sync these zones with DNS zone template.',
     'updateAddressPageTitle' => 'IP Address Traslation',
     'publicIp' => 'Public IP',
