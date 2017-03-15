4abd4769d3abd920b492ba4d0841d228c888fac2
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-03-15 21:16:14 +0700

    Update copyright year.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2017. Parallels IP Holdings GmbH.
 $messages = array(
     'indexPageTitle' => 'NAT Manager',
     'indexPageHint' => 'Select IP address and define public to private IP translation. In order to propagate changes to DNS zones of existing domains need to sync these zones with DNS zone template.',
